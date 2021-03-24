a4acadc89bbcceacd38a0a47bb7968aa3bc3bfd7
stachowski <stachowskibartosz96@gmail.com>
2021-03-24 20:43:33 +0100

    add fake user to store

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,14 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import * as event from './modules/event';
+import * as user from './modules/user';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
     event,
+    user,
   },
   state: {
     categories: ['job', 'education', 'fun', 'food', 'sport', 'nature'],
