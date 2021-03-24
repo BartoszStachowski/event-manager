92f65f70dcaa80119e76ed1d7ee355251896be98
stachowski <stachowskibartosz96@gmail.com>
2021-03-24 20:25:58 +0100

    display all details in EventShow

--- src/views/EventShow.vue
+++ src/views/EventShow.vue
@@ -6,14 +6,31 @@
       <h5>Organized by {{ event.organizer ? event.organizer : '' }}</h5>
       <h5>Category: {{ event.category }}</h5>
     </div>
+    <BaseIcon name="map"><h2>Location</h2></BaseIcon>
+    <address>{{ event.location }}</address>
+    <h2>Event details</h2>
+    <p>{{ event.description }}</p>
+    <h2>
+      Attendees
+      <span class="badge -fill-gradient">
+        {{ event.attendees ? event.attendees.length : 0 }}
+      </span>
+    </h2>
+    <ul class="list-group">
+      <li v-for="attendee in event.attendees" :key="attendee.id">
+        {{ attendee.name }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 import { mapState, mapActions } from 'vuex';
+import BaseIcon from '../components/BaseIcon';
 
 export default {
   name: 'EventShow',
+  components: { BaseIcon },
   props: ['id'],
   created() {
     this.fetchEvent(this.id);
@@ -26,7 +43,26 @@ export default {
 </script>
 
 <style scoped>
+.location {
+  margin-bottom: 0;
+}
+
+.location > .icon {
+  margin-left: 10px;
+}
+
 .event-header > .title {
   margin: 0;
+}
+
+.list-group {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+
+.list-group > .list-item {
+  padding: 1em 0;
+  border-bottom: solid 1px #e5e5e5;
 }
 </style>
