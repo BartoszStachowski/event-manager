176ec308551510df06e7dac10ce4726dace9dfd7
stachowski <stachowskibartosz96@gmail.com>
2021-03-24 20:41:01 +0100

    *create eventCreate component *add new path in router

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import EventList from '../views/EventList';
 import EventShow from '../views/EventShow';
+import EventCreate from '../views/EventCreate';
 
 Vue.use(VueRouter);
 
@@ -10,6 +11,11 @@ const routes = [
     path: '/',
     name: 'event-list',
     component: EventList,
+  },
+  {
+    path: '/event/create',
+    name: 'event-create',
+    component: EventCreate,
   },
   {
     path: '/event/:id',
---
+++ src/views/EventCreate.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h1>Create an Event</h1>
+    <form @submit.prevent="createEvent">
+      <label>Select a category</label>
+      <select>
+        <option v-for="cat in categories" :key="cat">{{ cat }}</option>
+      </select>
+      <h3>Name and describe your event</h3>
+      <div class="field">
+        <label>Title</label>
+        <input type="text" placeholder="Add an event title" />
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EventCreate',
+  data() {
+    return {
+      categories: this.$store.state.categories,
+      event: {},
+    };
+  },
+  methods: {
+    createEvent() {
+      console.log('xxx');
+    },
+  },
+};
+</script>
+
+<style scoped>
+.field {
+  margin-bottom: 24px;
+}
+</style>
